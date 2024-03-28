45a7a0ff8aa22f8c22bbe88fc4c7d945d73301df
ascappa <andreascappaticci24@gmail.com>
2024-03-28 12:56:22 +0100

    Co-authored-by: fake-code <fake-code@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const cities = ["London, UK", "Rome, Italy", "Berlin, Germany", "Barcelona, Spain"];
+const weekDays = ["Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica"]
+const WEATHER_KEY = "2e1c73811fbdb77294acb41f066d1786";
 const MAPBOX_TOKEN =
   "pk.eyJ1IjoiYXNjYXBwYSIsImEiOiJjbHVhY3hmZTAwZzU5MmttbWx0MnQycmlyIn0.AEkF1nFtCKq160tgyHls0Q";
 const cityDateEl = document.getElementById("city-date");
@@ -8,7 +10,9 @@ const cityWeatherNameEl = document.querySelector("#city-weather .weather");
 const citySearchFormEl = document.getElementById("citySearchForm");
 const cityTemperatureEl = document.querySelector("#city-weather .temperature");
 const cityListEl = document.getElementById("city-list");
-const cityWeatherIconEl = document.querySelector("#city-weather img")
+const cityWeatherIconEl = document.querySelector("#city-weather img");
+const dayWeatherEl = document.querySelector("#day-weather ul");
+const weekWeatherEl = document.querySelector("#week-weather ul");
 let timeId;
 timeId = setInterval(() => {
   const currentDate = new Date();
@@ -22,31 +26,59 @@ cities.forEach((city) => {
 citySearchFormEl.addEventListener("submit", async (e) => {
   e.preventDefault();
   try {
-  const formData = new FormData(e.target);
-  const res = await fetch(
-    `https://api.mapbox.com/geocoding/v5/mapbox.places/${formData.get(
-      "q"
-    )}.json?access_token=${MAPBOX_TOKEN}&limit=5&types=place`
-  );
-  const cities = await res.json();
-  const [lon, lat] = cities.features[0].center;
-  let locale = "it-IT";
-  fetchWeather(lon, lat).then((data) => {
-    cityWeatherNameEl.textContent = data.weather[0].main;
-    cityTemperatureEl.textContent = Math.round(data.main.temp) + "°";
-    cityWeatherIconEl.src = "./assets/" + data.weather[0].main.toLowerCase() + ".svg"
-    cityWeatherIconEl.alt = data.weather[0].main.toLowerCase()
-    cityNameEl.textContent = formData.get("q")
-    locale = data.sys.country.toLowerCase() + "-" + data.sys.country;
-    console.log(locale)
-  });
-  clearInterval(timeId);
-  timeId = setInterval(() => {
-    const currentDate = new Date();
-    cityTimeEl.textContent = currentDate.toLocaleTimeString(locale);
-  }, 1000); }
-  catch {
-    citySearchFormEl["q"].placeholder = "Città non valida"
+    const formData = new FormData(e.target);
+    const res = await fetch(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${formData.get(
+        "q"
+      )}.json?access_token=${MAPBOX_TOKEN}&limit=5&types=place`
+    );
+    const cities = await res.json();
+    const [lon, lat] = cities.features[0].center;
+    fetchWeather(lon, lat).then((data) => {
+      const currentDate = new Date();
+      cityWeatherNameEl.textContent = data.current.weather[0].main;
+      cityTemperatureEl.textContent = Math.round(data.current.temp) + "°";
+      cityWeatherIconEl.src = "./assets/" + data.current.weather[0].main.toLowerCase() + ".svg";
+      cityWeatherIconEl.alt = data.current.weather[0].main.toLowerCase();
+      cityNameEl.textContent = formData.get("q");
+      const hoursWeather = data.hourly.slice(0, 5);
+      const daysWeather = data.daily.slice(0,5)
+      const currentHour = Number(
+        currentDate
+          .toLocaleTimeString("it-IT", {
+            timeZone: data.timezone,
+          })
+          .split(":")[0]
+      );
+      const currentDayIndex = currentDate.getDay()
+      dayWeatherEl.innerHTML = "";
+      weekWeatherEl.innerHTML = ""
+      hoursWeather.forEach((hourData, idx) => {
+        dayWeatherEl.innerHTML += `<li><img src="${
+          "./assets/" + hourData.weather[0].main.toLowerCase() + ".svg"
+        }"><p>${((currentHour + idx) % 24).toString().padStart(2, "0") + ":00"}</p><p>${
+          Math.round(hourData.temp) + "°"
+        }</p></li>`;
+      });
+      daysWeather.forEach((dayData, idx) => {
+        console.log(currentDayIndex)
+        const currentWeekDay = weekDays[(currentDayIndex + idx) % 7]
+        weekWeatherEl.innerHTML += `<li><p>${currentWeekDay.slice(0, 3)}</p><img src="${
+          "./assets/" + dayData.weather[0].main.toLowerCase() + ".svg"
+        }"><p>${
+          Math.round(dayData.temp) + "°"
+        }</p></li>`;
+      });
+      clearInterval(timeId);
+      timeId = setInterval(() => {
+        const currentDate = new Date();
+        cityTimeEl.textContent = currentDate.toLocaleTimeString("it-IT", {
+          timeZone: data.timezone,
+        });
+      }, 1000);
+    });
+  } catch {
+    citySearchFormEl["q"].placeholder = "Città non valida";
   }
   citySearchFormEl["q"].value = "";
 });
@@ -54,7 +86,7 @@ citySearchFormEl["q"].addEventListener("input", async (e) => {
   console.log(e.target.value);
   cityListEl.innerHTML = "";
   if (e.target.value.length > 0) {
-    citySearchFormEl["q"].placeholder = "Cerca una città..."
+    citySearchFormEl["q"].placeholder = "Cerca una città...";
     const res = await fetch(
       `https://api.mapbox.com/geocoding/v5/mapbox.places/${e.target.value}.json?access_token=${MAPBOX_TOKEN}&limit=5&types=place`
     );
@@ -81,18 +113,13 @@ cityDateEl.textContent = cityDate;
 
 async function fetchWeather(lon = 70, lat = 70) {
   const resW = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?lat=" +
-      lat +
-      "&lon=" +
-      lon +
-      "&limit=5&units=metric&appid=2e1c73811fbdb77294acb41f066d1786"
-      
+    `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${WEATHER_KEY}&units=metric`
   );
   const dataW = await resW.json();
   console.log(dataW);
   return dataW;
 }
 fetchWeather().then((data) => {
-  cityWeatherNameEl.textContent = data.weather[0].main;
-  cityTemperatureEl.textContent = Math.round(data.main.temp) + "°";
+  cityWeatherNameEl.textContent = data.current.weather[0].main;
+  cityTemperatureEl.textContent = Math.round(data.current.temp) + "°";
 });
